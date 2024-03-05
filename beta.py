73f9c448a233bf45a600417a8ab6ed11fe540726
Koukunn_ <testbing@outlook.jp>
2024-03-06 08:54:48 +0900

    AI_key

--- beta.py
+++ beta.py
@@ -111,7 +111,7 @@ async def ask(request: Request):
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}")
         try:
-          response = await g4f.ChatCompletion.create_async(model="gpt-3.5-turbo", messages=conversation_history[user_id])
+          response = await g4f.ChatCompletion.create_async(model="gemini-pro",provider=g4f.Provider.GeminiPro,api_key="AIzaSyDHCVkGkQ0d5lQ230ssHzf3rg2XZBjNCZM", messages=conversation_history[user_id])
         except Exception as e:
          if 'candidates' in str(e):
             logging.error("A 'candidates' error occurred. Please check your input and try again.")
