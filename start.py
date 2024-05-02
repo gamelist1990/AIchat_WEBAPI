20ab93d1c560d46a9d9faaab0f5ec5db69e77199
Koukunn_ <testbing@outlook.jp>
2024-05-02 10:32:48 +0900

    updae

--- start.py
+++ start.py
@@ -6,15 +6,15 @@ import subprocess
 
 print("SERVERの起動準備中・・・")
 
-subprocess.check_call(['pip','install','-r','requirements.txt'])
-print("G4Fのインストール")
-subprocess.check_call(['pip','install','-U','g4f[all]'])
-command = ['pip', 'uninstall', 'undetected-chromedriver']
-process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-output, error = process.communicate(input=b'Y\n')
+#subprocess.check_call(['pip','install','-r','requirements.txt'])
+#print("G4Fのインストール")
+#subprocess.check_call(['pip','install','-U','g4f[all]'])
+#command = ['pip', 'uninstall', 'undetected-chromedriver']
+#process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#output, error = process.communicate(input=b'Y\n')
 
-print("Output:", output.decode())
-print("Error:", error.decode())
+#print("Output:", output.decode())
+#print("Error:", error.decode())
 
 print("SERVER起動完了")
 
