ee1c68ec5acd34ec0e98e8a3ab14b084f2bd216f
Koukunn_ <testbing@outlook.jp>
2024-05-02 10:48:49 +0900

    余計なprintを削除

--- start.py
+++ start.py
@@ -1,11 +1,6 @@
 import os
 import subprocess
 
-
-
-
-print("SERVERの起動準備中・・・")
-
 #subprocess.check_call(['pip','install','-r','requirements.txt'])
 #print("G4Fのインストール")
 #subprocess.check_call(['pip','install','-U','g4f[all]'])
@@ -13,10 +8,6 @@ command = ['pip', 'uninstall', 'undetected-chromedriver']
 process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 output, error = process.communicate(input=b'Y\n')
 
-print("Output:", output.decode())
-print("Error:", error.decode())
-
-print("SERVER起動完了")
 #subprocess.check_call(['pip','list'])
 # uvicornを起動
 subprocess.check_call(['uvicorn', 'beta:app', '--reload', '--host', '0.0.0.0', '--port', '5000'])
