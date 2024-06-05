e5d2aeeecacff3ed83c1d1930e664796cfee1cd4
Koukunn_ <testbing@outlook.jp>
2024-06-05 15:26:22 +0900

    kyuukyo

--- beta.py
+++ beta.py
@@ -182,14 +182,14 @@ async def ask(request: Request):
     try:
      response = await g4f.ChatCompletion.create_async(
         provider=g4f.Provider.You,
-        model="gpt-4-turbo",
+        model="gpt-3.5-turbo",
         api_key=set_cookies_dir(cookies_dir),
         messages=messages,
     )
     except Exception as e:
         logging.error(f"Error occurred: {str(e)}")
         # If the ask function fails, return an error message
-        response = g4f_gemini(user_id,text)
+        response = f"現在Youプロバイダーでエラーが発生しましたエラー内容:{str(e)}"
         return JSONResponse(content={"response": str(e)}, status_code=500)
 
     try:
