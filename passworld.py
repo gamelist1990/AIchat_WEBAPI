416bafc86922b668037809881e0ce6b6bad950ab
Koukunn_ <testbing@outlook.jp>
2024-06-20 17:09:46 +0900

    update 安定版

--- passworld.py
+++ passworld.py
@@ -5,3 +5,4 @@ password = "test"  # ここに設定したいパスワードを入力
 hashed_password = pwd_context.hash(password)
 print(f"hash passworld: {hashed_password}")
 
+pas = " $2b$12$0TsIwyv50SLpZRc4SSMjQeFWPxGKFhTvVx1RCAKLNTk5HFUMlqqnq"
